83cbe53be86a2aa1abed706fa4f174e87c187c9d
Wilson G <wilson22garcia@gmail.com>
2021-02-11 23:34:12 +0000

    Added test and continuous integration

---
+++ test.py
@@ -0,0 +1,8 @@
+import unittest
+from main import display
+class TestIsOdd(unittest.TestCase):
+
+    def test_display(self):
+        self.assertTrue(True)
+if __name__ == '__main__':
+    unittest.main()
